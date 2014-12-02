5e30d904a4defe497c31f68153fe4c2bd66e9632
Kazunori Kajihiro <likerichie@gmail.com>
2014-12-02 18:26:25 +0900

    Ensure deploy directory exists

--- dotenv/recipes/default.rb
+++ dotenv/recipes/default.rb
@@ -1,4 +1,10 @@
 node[:deploy].each do |application, deploy|
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
   dotenv_template do
     application application
     deploy deploy
