4a4702874cf5ec4978d5d961ac2bc771c87697db
Richard Ramsden <richard@rramsden.ca>
2014-12-11 14:30:54 +0900

    Add `pool_size` setting for easier configuration

--- delayed_job/attributes/default.rb
+++ delayed_job/attributes/default.rb
@@ -1,4 +1,16 @@
+include_attribute "deploy"
+
 default[:delayed_job][:timeout] = 60
 default[:delayed_job][:bin] = "bin/delayed_job"
 default[:delayed_job][:suffix] = ""
 default[:delayed_job][:options] = ""
+
+node[:deploy].each do |application, deploy|
+  if deploy[:delayed_job][:pool_size]
+    default[:delayed_job][:workers] = deploy[:delayed_job][:pool_size].times.map{ {} }
+  end
+
+  if deploy[:delayed_job][:workers]
+    default[:delayed_job][:workers] = deploy[:delayed_job][:workers]
+  end
+end
