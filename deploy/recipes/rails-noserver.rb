ed5e6ba66d2f2cc9e5c492f4fa2e2788b4c46ad3
Richard Ramsden <richard@rramsden.ca>
2014-12-11 15:40:52 +0900

    Use old ruby syntax

--- deploy/recipes/rails-noserver.rb
+++ deploy/recipes/rails-noserver.rb
@@ -2,7 +2,7 @@ unless node.recipes.include?('deploy::rails')
   include_recipe 'deploy::rails'
 
   node[:deploy].each do |application, deploy|
-    r = resources(template: "#{deploy[:deploy_to]}/shared/scripts/unicorn")
+    r = resources(:template => "#{deploy[:deploy_to]}/shared/scripts/unicorn")
     r.source('unicorn.service.disabled.erb')
   end
 end
